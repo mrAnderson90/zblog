cac3385ecbef71c34e12a5e013497fb14111d085
mrAnderson90 <tyler.andreev@mail.ru>
2025-09-15 15:31:05 +0300

    Add main Blog page

--- module/Blog/src/Blog/Controller/IndexController.php
+++ module/Blog/src/Blog/Controller/IndexController.php
@@ -2,13 +2,33 @@
 
 namespace Blog\Controller;
 
-use Zend\Mvc\Controller\AbstractActionController;
-use Zend\View\Model\ViewModel;
+use Application\Controller\BaseController as BaseController;
 
-class IndexController extends AbstractActionController
+use DoctrineORMModule\Paginator\Adapter\DoctrinePaginator as DoctrineAdapter;
+use Doctrine\ORM\Tools\Pagination\Paginator as ORMPaginator;
+use Zend\Paginator\Paginator;
+
+use DoctrineORMModule\Form\Annotation\AnnotationBuilder;
+use DoctrineModule\Stdlib\Hydrator\DoctrineObject as DoctrineHydrator;
+
+
+class IndexController extends BaseController
 {
     public function indexAction()
     {
-        return new ViewModel();
+      $query = $this->getEntityManager()->createQueryBuilder();
+      $query
+        ->add('select', 'a')
+        ->add('from', 'Blog\Entity\Article a')
+        ->add('where', 'a.isPublic=1')
+        ->add('orderBy', 'a.id ASC');
+
+      $adapter = new DoctrineAdapter(new ORMPaginator($query));
+
+      $paginator = new Paginator($adapter);
+      $paginator->setDefaultItemCountPerPage(4);
+      $paginator->setCurrentPageNumber((int) $this->params()->fromQuery('page', 2));
+
+      return array('articles' => $paginator);
     }
 }
--- module/Blog/src/Blog/Entity/Article.php
+++ module/Blog/src/Blog/Entity/Article.php
@@ -204,4 +204,21 @@ class Article
         $article = mb_substr($article, 0, 15, 'UTF-8') . '...';
         return $article;
     }
+
+    public function getShortArticleForBlog()
+    {
+      $article = $this->getShortArticle();
+      if (empty($article)) {
+        $article = $this->getArticle();
+      }
+
+      return $article;
+    }
+
+    public function getFullArticle()
+    {
+      $article = $this->getShortArticle() . $this->getArticle();
+
+      return $article;
+    }
 }
